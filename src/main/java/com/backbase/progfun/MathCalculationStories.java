ba84097384869da9abbc0b4dfbc25f4bf71205b4
Zoltan Altfatter <altfatterz@gmail.com>
2014-01-14 15:04:17 +0100

    add more configuration

--- src/main/java/com/backbase/progfun/MathCalculationStories.java
+++ src/main/java/com/backbase/progfun/MathCalculationStories.java
@@ -2,39 +2,27 @@ package com.backbase.progfun;
 
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.io.CodeLocations;
-import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
-import org.jbehave.core.io.UnderscoredCamelCaseResolver;
 import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.reporters.CrossReference;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
-import org.jbehave.core.steps.SilentStepMonitor;
 
 import java.util.List;
 
 import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
 import static org.jbehave.core.reporters.Format.*;
 
-public class MathCalculationStory extends JUnitStories {
+public class MathCalculationStories extends JUnitStories {
 
     @Override
     public Configuration configuration() {
-
-        Class<? extends MathCalculationStory> embedderClass = this.getClass();
-
         return new MostUsefulConfiguration()
-                .useStoryLoader(new LoadFromClasspath(embedderClass.getClassLoader()))
-                .useStoryPathResolver(new UnderscoredCamelCaseResolver())
                 .useStoryReporterBuilder(new StoryReporterBuilder()
-                        .withCodeLocation(CodeLocations.codeLocationFromClass(embedderClass))
                         .withDefaultFormats()
                         .withFormats(CONSOLE, HTML, XML)
-                        .withCrossReference(new CrossReference()))
-                .useStepMonitor(new SilentStepMonitor());
-
+                        .withCrossReference(new CrossReference()));
     }
 
     @Override
